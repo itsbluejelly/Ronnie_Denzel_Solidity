6809689843efc30fc4f3d292e53b9c037134926b
itsbluejelly <ronniedenzel0@gmail.com>
2024-03-31 20:46:02 +0300

    improved the MappingsContract

--- contracts/Mappings.sol
+++ contracts/Mappings.sol
@@ -8,6 +8,9 @@ contract MappingsContract{
     uint cooldownTime;
     uint countIndex = 0;
 
+    // DECLARING EVENTS
+    event NftMined(address indexed _address, uint indexed nftID);
+
     // DECLARING A CONSTRUCTOR TO SET COOLDOWN TIME
     constructor(){
         cooldownTime = 5 seconds;
@@ -16,10 +19,10 @@ contract MappingsContract{
     // A FUNCTION TO MINT AN NFT
     function mintNft() external{
         require(block.timestamp > lastMiningTime + cooldownTime, "You need to mine after the cooldown");
+        uint nftID = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, countIndex)));
         
-        addressesToNfts[msg.sender].push(
-            uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, countIndex)))
-        );
+        addressesToNfts[msg.sender].push(nftID);
+        emit NftMined(msg.sender, nftID);
         
         countIndex++;
         lastMiningTime = block.timestamp;
