16ac7e2f4bd0dfec16e457348a9282aefb150030
itsbluejelly <ronniedenzel0@gmail.com>
2024-04-02 19:42:10 +0300

    added transaction capabilities

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -11,11 +11,22 @@ contract CounterContract is Ownable(msg.sender), ICounter{
     // DECLARING VARIABLES
     uint public count = 0;
     bool public counterActivated = true;
+    uint public transactionCost = 0.01 ether;
+
+    // A CONSTRUCTOR THAT DEPOSITS MONEY TO THE CONTRACT
+    constructor() payable {
+        require(msg.value >= 0.02 ether, "Minimum value to deposit to the contract is 0.02 ether");
+    }
 
     // A MODIFIER TO CHECK IF COUNT CAN BE UPDATED
     modifier onlyCounterActivated{
         require(counterActivated == true, "Counter can be updated only when activated");
         _;
+    }
+
+    // A FUNCTION TO SET THE TRANSACTION COST
+    function setTransactionCost(uint _value) external onlyOwner{
+        transactionCost = _value;
     }
 
     // A FUNCTION TO UPDATE THE COUNTER ACTIVATED
@@ -24,11 +35,20 @@ contract CounterContract is Ownable(msg.sender), ICounter{
     }
 
     // A FUNCTION TO INCREASE OR DECREASE COUNT
-    function increaseCount() external onlyCounterActivated{
+    function increaseCount() external payable onlyCounterActivated{
+        require(msg.value == transactionCost);
         count++;
     }
 
-    function decreaseCount() external onlyCounterActivated{
+    function decreaseCount() external payable onlyCounterActivated{
+        require(msg.value == transactionCost);
         count--;
+    }
+
+    // FUNCTION TO WITHDRAW FUNDS FROM THE CONTRACT
+    function withdrawFunds() external onlyOwner{
+        address payable currentOwner = payable (owner());
+        uint contractBalance = address(this).balance;
+        currentOwner.transfer(contractBalance);
     }
 }
--- interfaces/ICounter.sol
+++ interfaces/ICounter.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.25;
 
 interface ICounter{
+    function setTransactionCost(uint _value) external ;
     function setCounterActivated(bool _counterActivated) external;
-    function increaseCount() external;
-    function decreaseCount() external;
+    function increaseCount() external payable ;
+    function decreaseCount() external payable ;
 }
